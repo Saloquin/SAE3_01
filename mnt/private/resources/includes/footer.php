f90db2e0c23911787f5e143e5f69744d7861b438
Nathan CHIRAC <chiracnathan59@gmail.com>
2025-01-07 17:16:15 +0100

    FIX : home.blade.php corrigé, le lien css marche

--- mnt/private/resources/includes/footer.php
+++ mnt/private/resources/includes/footer.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8" />
     <title>S3.01</title>
-    <link rel="stylesheet" href="{{URL::to('css/app.css')}}">
 </head>
 <body>
     <footer id="footer">
--- mnt/private/routes/web.php
+++ mnt/private/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return 'bonjour';
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/test', function () {
     
     return 'au revoir';
