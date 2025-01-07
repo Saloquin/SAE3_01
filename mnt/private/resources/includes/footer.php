feb4af89115b53218a8be4dfcf1e36a43d10a845
Nathan CHIRAC <chiracnathan59@gmail.com>
2025-01-07 13:46:31 +0100

    FEAT : Ajout header, footer fonctionnel + typographie + logo

---
+++ mnt/private/resources/includes/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>S3.01</title>
+    <link rel="stylesheet" href="./styles/style.css" >
+</head>
+<body>
+    <footer id="footer">
+        <small> Copyright @ . Tous droits réservés </small>
+    </footer>
+</body>
+</html>
---
+++ mnt/private/resources/includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>S3.01</title>
+    <link rel="stylesheet" href="../css/style/style.css" >
+</head>
+<body>
+    <div class="header">
+        <div id="logo">
+            <a href="#">
+                <figure>
+                    <img src="../css/images/logo.png" alt="Home">
+                </figure>
+            </a>
+        </div>
+        <div  id="title">
+            <h1> Journal de plongée </h1>
+        </div>
+        <div id="profile">
+            <a href="#">
+                <figure>
+                    <img src="../css/images/profile.png" alt="Profile">
+                    <p id="account_name">  </p>
+                </figure>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
