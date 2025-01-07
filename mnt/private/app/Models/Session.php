9581c021e66f14154be4a1187714fb70eda2e497
Maceo <maceod23@gmail.com>
2025-01-07 13:22:38 +0100

    model insert session

--- mnt/private/app/Models/Session.php
+++ mnt/private/app/Models/Session.php
@@ -10,10 +10,10 @@ class Session extends Model
 {
     use HasFactory;
 
-    public static function insertSession() {
-        // $cou_id = select max + 1
+    public static function insertSession($for_id, $cou_date, $uti_id_elv1, $uti_id_elv2, $uti_id_init) {
+        $cou_id = intval(DB::select("select max(cou_id) as cou_id from COURS")[0]->cou_id) + 1;
 
-        DB::insert("insert into COURS (cou_id, for_id, cou_date) values (?, ?, str_to_date(?, '%d-%m-%Y'))", [10, 1, '07-01-2025']);
-        // DB::insert("insert into GROUPE (cou_id, cou_date) values (?, str_to_date(?, '%d-%m-%Y'))", [1, '07-01-2025']);
+        DB::insert("insert into COURS (cou_id, for_id, cou_date) values (?, ?, str_to_date(?, '%d-%m-%Y'))", [$cou_id, $for_id, $cou_date]);
+        DB::insert("insert into GROUPE (cou_id, uti_id_elv1, uti_id_elv2, uti_id_init) values (?, ?, ?, ?)", [$cou_id, $uti_id_elv1, $uti_id_elv2, $uti_id_init]);
     }
 }
