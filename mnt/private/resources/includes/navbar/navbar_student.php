bc252e22428e138d25d6c18770a8a330543e225f
Isaac <isaac.delauney@etu.unicaen.fr>
2025-01-08 11:41:31 +0100

    added the navbar displays

---
+++ mnt/private/resources/includes/navbar/navbar_student.php
@@ -0,0 +1,4 @@
+<div class="flex flex-row bg-[#223A5D] justify-around py-[1vh]">
+    <button class="triomphe lg:text-[1vw] text-[3vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Emploi du temps</button>
+    <button class="triomphe lg:text-[1vw] text-[3vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Mes compétences</button>
+</div>
