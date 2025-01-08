1ae60a0c65a38d87fc16feceaed6588cc8347c15
Nathan CHIRAC <chiracnathan59@gmail.com>
2025-01-08 13:47:49 +0100

    FEAT : Controllers template ajoutés

---
+++ mnt/private/app/Http/Controllers/Nav/EditTraining.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+Class EditTraining extends Controller{
+
+    public function show(){
+        return view('edittraining');
+    }
+
+}
---
+++ mnt/private/app/Http/Controllers/Nav/SessionManagement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+Class SessionManagement extends Controller{
+
+    public function show(){
+        return view('sessionmanagement');
+    }
+
+}
