87d0b19d335c7598f3b6227f3d0a13ebd1d3e14b
Maceo <maceod23@gmail.com>
2025-01-09 11:44:51 +0100

    la page evaluation seance pour les initiateurs est reparee NORMALEMENT mais il y a un seul input commentaire alors que dans la base il y a un commentaire par aptitude

--- mnt/private/app/Models/Lesson.php
+++ mnt/private/app/Models/Lesson.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Mastery;
 
 class Lesson extends Model
@@ -90,8 +89,9 @@ class Lesson extends Model
             return array();
         }
 
-        $skillsIds = DB::select("select apt_id from MAITRISER where cou_id = ? and uti_id = ?", [$cou_id, $uti_id]);
+        $skills = DB::select("select * from MAITRISER join APTITUDE using(apt_id) where cou_id = ? and uti_id = ?", [$cou_id, $uti_id]);
 
+        /*
         $skills = array();
 
         foreach ($skillsIds as $row) {
@@ -99,6 +99,7 @@ class Lesson extends Model
 
             array_push($skills, $skill);
         }
+        */
         
         return $skills;
     }
