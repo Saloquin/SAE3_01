155dddaa1e2c27a9e492da9aaff2190fccab1adb
Thomas Roger <saloquin24@gmail.com>
2025-01-09 17:10:57 +0100

    mail

--- mnt/private/resources/views/emails/user_passwordlost.blade.php
+++ mnt/private/resources/views/emails/user_passwordlost.blade.php
@@ -7,6 +7,7 @@
     <h1>Bonjour, {{ $user['name'] }}</h1>
     <p>Voici votre mot de passe de récupération.</p>
     <p>Email : {{ $user['email'] }}</p>
+    <p>Licence  : {{ $user['licence'] }}</p>
     <p>Mot de passe temporaire : {{ $user['password'] }}</p>
 </body>
 </html>
