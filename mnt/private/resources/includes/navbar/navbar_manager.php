e0616da0682d46a2ee66fbb8f5fa70a4d475c6e4
Nathan CHIRAC <chiracnathan59@gmail.com>
2025-01-09 16:04:07 +0100

    FEAT : responsive : gestion-utilisateurs + différents navbar

--- mnt/private/resources/includes/navbar/navbar_manager.php
+++ mnt/private/resources/includes/navbar/navbar_manager.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row bg-[#223A5D] justify-around py-[1vh] w-full">
+<div class="flex flex-row bg-[#223A5D] justify-around py-[1vh] w-full navbar">
         <a href="/responsable-formation"><button class="mr-[1vw] triomphe lg:text-[1vw] text-[3vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Emploi du temps</button></a>
         <a href="/responsable-formation/details-formation"><button class="mr-[1vw] triomphe lg:text-[1vw] text-[3vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Détails de la formation</button></a>
         <a href="/responsable-formation/gestion-aptitude"><button class="mr-[1vw] triomphe lg:text-[1vw] text-[3vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Synthèse formation</button></a>
--- mnt/private/resources/views/usermanagement.blade.php
+++ mnt/private/resources/views/usermanagement.blade.php
@@ -17,7 +17,7 @@
 
     <div class=" flex flex-col lg:w-auto w-[90%]">
         <div class="flex lg:flex-row flex-col lg:justify-between items-center mb-[1vh]">
-            <p class="triomphe text-[3vw] lg:text-[1.3vw]">Liste des utilisateurs</p>
+            <p class="triomphe text-[3vw] lg:text-[1.3vw] user-title">Liste des utilisateurs</p>
             <form method="GET" action="{{ route('directeur.ajouter-utilisateur') }}">
                 <button
                     class=" triomphe lg:text-[1vw] text-[3vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Créer
@@ -28,13 +28,13 @@
         <table class="border-[0.1vw] rounded-[0.5vw] border-[#1962A1] mb-[10vh] border-separate border-spacing-[0.5vw]">
             <thead>
                 <tr>
-                    <th class="table_header text-[3vw] lg:text-[1vw]">Licence</th>
-                    <th class="table_header text-[3vw] lg:text-[1vw]">Nom</th>
-                    <th class="table_header text-[3vw] lg:text-[1vw]">Prénom</th>
-                    <th class="table_header text-[3vw] lg:text-[1vw] lg:table-cell hidden">Mail</th>
-                    <th class="table_header text-[3vw] lg:text-[1vw]">Niveau</th>
-                    <th class="table_header text-[3vw] lg:text-[1vw] lg:table-cell hidden">Certif.</th>
-                    <th class="table_header text-[3vw] lg:text-[1vw] lg:table-cell hidden">Nais.</th>
+                    <th class="table_header text-[3vw] lg:text-[1vw] triomphe">Licence</th>
+                    <th class="table_header text-[3vw] lg:text-[1vw] triomphe">Nom</th>
+                    <th class="table_header text-[3vw] lg:text-[1vw] triomphe">Prénom</th>
+                    <th class="table_header text-[3vw] lg:text-[1vw] lg:table-cell hidden triomphe">Mail</th>
+                    <th class="table_header text-[3vw] lg:text-[1vw] triomphe">Niveau</th>
+                    <th class="table_header text-[3vw] lg:text-[1vw] lg:table-cell hidden triomphe">Certif.</th>
+                    <th class="table_header text-[3vw] lg:text-[1vw] lg:table-cell hidden triomphe">Nais.</th>
                 </tr>
             </thead>
             <tbody>
@@ -55,7 +55,7 @@
                                     class="lg:table-cell hidden triomphe lg:text-[0.8vw] text-[2vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Gérer
                                     l'utilisateur</button>
                                 <button type="submit"
-                                        class="lg:hidden triomphe lg:text-[0.8vw] text-[2vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white">Gérer
+                                        class="lg:hidden triomphe lg:text-[0.8vw] text-[2vw] rounded-[0.25vw] bg-[#1962A1] px-[1vw] py-[0.8vh] text-white manage-button">Gérer
                                     </button>
                             </form>
                         </td>
